cd571884ee6a6c4ac2e3c95225630bb519067d0d
Zach H <zhensler12@gmail.com>
2023-12-08 22:18:27 -0500

    Add base url to config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import preact from '@preact/preset-vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
-  root: './gallery',
+  base: 'https://zach-hensler.github.io/gallery',
+  root: './',
   build: {
     outDir: 'build',
   },
