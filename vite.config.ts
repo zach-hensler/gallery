1fc7a97e2dec16834a0c1d993388652a58bc0901
Zach H <zhensler12@gmail.com>
2023-12-08 19:56:25 -0500

    Update page root

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import preact from '@preact/preset-vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
-  root: './',
+  root: './gallery',
   build: {
     outDir: 'build',
   },
