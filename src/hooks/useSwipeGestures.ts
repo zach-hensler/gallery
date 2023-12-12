4cdd739c6b67a6e229a8688f8e05246bcf82f554
Zach H <zhensler12@gmail.com>
2023-12-11 21:07:17 -0500

    Add Swipe Support

---
+++ src/hooks/useSwipeGestures.ts
@@ -0,0 +1,61 @@
+import {useCallback, useEffect, useRef} from "preact/hooks";
+
+interface SwipeGestureCallbacks {
+    handleLeftSwipe: () => void
+    handleRightSwipe: () => void
+    handleUpSwipe: () => void
+    handleDownSwipe: () => void
+}
+
+/**
+ * Calls the associated callbacks when a swipe is registered.
+ * Based off of this stack overflow post: https://stackoverflow.com/a/23230280
+ * @param callbacks - Function called when a swipe occurs 
+ */
+export const useSwipeGestures = (callbacks: SwipeGestureCallbacks) => {
+    const xDown = useRef<number|null>(null)
+    const yDown = useRef<number|null>(null)
+
+    const handleTouchStart = useCallback((event: TouchEvent) => {
+        const firstTouch = event.touches[0];
+        xDown.current = firstTouch.clientX;
+        yDown.current = firstTouch.clientY;
+    }, []);
+
+    const handleTouchMove = useCallback((event: TouchEvent) => {
+
+        const xUp = event.changedTouches[0].clientX;
+        const yUp = event.changedTouches[0].clientY;
+
+        if ( !xDown.current || !yDown.current ) return;
+        const xDiff = xDown.current - xUp;
+        const yDiff = yDown.current - yUp;
+
+        if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+            if ( xDiff > 0 ) {
+                callbacks.handleLeftSwipe();
+            } else {
+                callbacks.handleRightSwipe();
+            }
+        } else {
+            if ( yDiff > 0 ) {
+                callbacks.handleUpSwipe();
+            } else {
+                callbacks.handleDownSwipe();
+            }
+        }
+        /* reset values */
+        xDown.current = null;
+        yDown.current = null;
+    }, [callbacks.handleDownSwipe, callbacks.handleUpSwipe, callbacks.handleLeftSwipe, callbacks.handleRightSwipe]);
+
+    useEffect(() => {
+        document.addEventListener('touchstart', handleTouchStart);
+        document.addEventListener('touchend', handleTouchMove);
+
+        return () => {
+            document.removeEventListener('touchstart', handleTouchStart);
+            document.removeEventListener('touchend', handleTouchMove);
+        }
+    }, [handleTouchStart, handleTouchMove])
+}
